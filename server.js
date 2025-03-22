4aeff466b87e8257be556378b97d00ba472b35c4
EvyaDev <106523004+EvyaDev@users.noreply.github.com>
2025-03-23 00:55:22 +0200

    add

--- server.js
+++ server.js
@@ -1,18 +1,28 @@
 const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
-const ordersRoutes = require("./routes/orders");
+const db = require("./db"); // חיבור למסד נתונים
 
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
-// שימוש בנתיבים להזמנות
-app.use("/api/orders", ordersRoutes);
+const checkDbConnection = () => {
+    db.getConnection((err, connection) => {
+        if (err) {
+            console.error('Error connecting to the database:', err);
+            return;
+        }
+        console.log('Connected to the database!');
+        connection.release(); // שחרר את החיבור
+    });
+};
+
+checkDbConnection();
 
 const PORT = process.env.PORT || 4000;
+
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
-    app.get
 });
-app.get('/', (req, res) => { res.send("Welcome to API server") });
+
