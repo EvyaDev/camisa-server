2226521a34dc70120ae414fdc2e1daeab7c4be08
EvyaDev <106523004+EvyaDev@users.noreply.github.com>
2025-03-22 23:37:29 +0200

    add

--- server.js
+++ server.js
@@ -13,4 +13,6 @@ app.use("/api/orders", ordersRoutes);
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
+    app.get
 });
+app.get('/', (req, res) => { res.send("Welcome to API server") });
