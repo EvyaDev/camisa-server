938e9f7ca774465431d73731402c72b606a09c85
EvyaDev <106523004+EvyaDev@users.noreply.github.com>
2025-03-22 23:26:45 +0200

    first commit

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const ordersRoutes = require("./routes/orders");
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+
+// שימוש בנתיבים להזמנות
+app.use("/api/orders", ordersRoutes);
+
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
