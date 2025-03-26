e6a1d63b4af14e1dca7491865fbfbbc10f364453
EvyaDev <106523004+EvyaDev@users.noreply.github.com>
2025-03-26 20:50:33 +0200

    test render

--- dbConnection.js
+++ dbConnection.js
@@ -1,18 +1,25 @@
-const mysql = require('mysql2');
+// const mysql = require('mysql2');
 
-const con = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'camisa',
-});
+// // const con = mysql.createConnection({
+// //     host: 'localhost',
+// //     user: 'root',
+// //     password: '',
+// //     database: 'camisa',
+// // });
 
-con.connect(err => {
-    if (err) {
-        throw err;
-    }
+// const con = mysql.createConnection({
+//     host: '62.219.58.195',
+//     user: 'admin_camisa',
+//     port: 3306,
+//     password: '=!MMD=2X7P(+',
+//     database: 'loccacoi_camisa-db',
+// });
 
-    console.log("Connection established");
-});
+// con.connect(err => {
+//     if (err) {
+//         throw err;
+//     }
+//     console.log("Connection established");
+// });
 
-module.exports = con;
+// module.exports = con;
